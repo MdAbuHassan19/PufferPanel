57ed4d790aabc73d113457ea3205a00c22230ffa
MdAbuHassan19 <mdabuhassan518@gmail.com>
2025-02-17 21:00:48 +0530

    Update Puffer_Panel_Installer.sh

--- Puffer_Panel_Installer.sh
+++ Puffer_Panel_Installer.sh
@@ -4,7 +4,7 @@ main ()
   # update apt on this system
    apt update
    apt upgrade -y
-   wget https://github.com/MdAbuHassan19/PufferPanel/blob/main/install_panel.sh
+   git clone https://github.com/MdAbuHassan19/PufferPanel/blob/main/install_panel.sh
    bash install_panel.sh
    apt install pufferpanel
    pufferpanel user add
